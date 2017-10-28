024e74558e42a48be31dbe6a5c6c79bdd6111629
razone24 <32982904+razone24@users.noreply.github.com>
2017-10-28 20:08:54 +0300

    Update termometru.ino

--- termometru.ino
+++ termometru.ino
@@ -1,24 +1,24 @@
 #include <LiquidCrystal.h>
 #include "Wire.h"
      #define DS1307_ADDRESS 0x68
-     byte zero = 0x00;       
- LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
- const int inPin = 0;                   
+     byte zero = 0x00; //rezolva eroarea #527       
+ LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //Pinii la care este conectat LCD-ul
+ const int inPin = 0;                   // A0 este pinul unde este conectat senzorul de temperatura
  void setup()
  {
   lcd.begin(16,2);
   Wire.begin();
        Serial.begin(9600);
-       setDateTime();
+       setDateTime(); //Apelarea functiei care citeste si afiseaza ceasul
  }
  void loop()
  {
    float sumaTemperatura;
    lcd.setCursor(0,1);
  for (int i =0; i<10; i++) {
-          int reading = analogRead(0);
+          int reading = analogRead(0);//Citeste de la senzorul de temperatura temperatura in volti
           float voltage = reading * 5.0;
-          voltage /= 1024.0;
+          voltage /= 1024.0;                  //Transforma temperatura din volti in grade celsius
           float temperatureCelsius = (voltage - 0.5) * 10 ;
           sumaTemperatura = sumaTemperatura + temperatureCelsius;
  }
@@ -26,7 +26,7 @@
    lcd.setCursor(0,0);
    printDate();
    lcd.setCursor(10,1);
-   lcd.print(sumaTemperatura);
+   lcd.print(sumaTemperatura); //Afiseaza pe LCD temperatura in grade celsius
    lcd.print("C");
    delay(1000);
  }
@@ -34,12 +34,12 @@
   byte second =00; //0-59
   byte minute =50; //0-59
   byte hour =16; //0-23
-  byte weekDay =3; //1-7
+  byte weekDay =3; //1-7             //Potrivirea ceasului
   byte monthDay =25; //1-31
   byte month =10; //1-12
   byte year  =17; //0-99
 Wire.beginTransmission(DS1307_ADDRESS);
-Wire.write(zero); //stop Oscillator
+Wire.write(zero); //Opreste oscilatorul
 Wire.write(decToBcd(second));
 Wire.write(decToBcd(minute));
 Wire.write(decToBcd(hour));
@@ -47,33 +47,33 @@ Wire.write(decToBcd(weekDay));
 Wire.write(decToBcd(monthDay));
 Wire.write(decToBcd(month));
 Wire.write(decToBcd(year));
-  Wire.write(zero); //start
+  Wire.write(zero); //Porneste functionarea ceasului in functie de ora potrivita
   Wire.endTransmission();
 }
 byte decToBcd(byte val){
-
+//Transforma numerele zecimale in numere in numere binare
   return ( (val/10*16) + (val%10) );
 }
 byte bcdToDec(byte val)  {
-
+// Transforma numerele binare in numere zecimale
   return ( (val/16*10) + (val%16) );
 }
 void printDate(){
-  
+  // Reseteaza registrul pointerului
   Wire.beginTransmission(DS1307_ADDRESS);
   Wire.write(zero);
   Wire.endTransmission();
   Wire.requestFrom(DS1307_ADDRESS, 7);
   int second = bcdToDec(Wire.read());
   int minute = bcdToDec(Wire.read());
-  int hour = bcdToDec(Wire.read() & 0b111111); 
-  int weekDay = bcdToDec(Wire.read()); 
+  int hour = bcdToDec(Wire.read() & 0b111111); //timpul in formatul de 24 de ore
+  int weekDay = bcdToDec(Wire.read()); //zilele saptamanii Luni-Duminica
   int monthDay = bcdToDec(Wire.read());
   int month = bcdToDec(Wire.read());
   int year = bcdToDec(Wire.read());
-  lcd.print(monthDay);
+  lcd.print(monthDay); //Afiseaza zilele saptamanii Luni-Duminica
   lcd.print("-");
-  switch (month) {
+  switch (month) { //Lunile anului Ian-Dec
     case 1:
     lcd.print("ian");
     break;
@@ -112,17 +112,17 @@ void printDate(){
     break;
   }
   lcd.print("-");
-  lcd.print(year);
+  lcd.print(year); //Afisarea anului
   lcd.print(" ");
   lcd.setCursor(0,1);
-  lcd.print(hour);
+  lcd.print(hour);  //Afisarea orei
   lcd.print(":");
-  lcd.print(minute);
+  lcd.print(minute);//Afisarea minutelor
   lcd.print(":");
-  lcd.print(second);
+  lcd.print(second); //Afisarea secundelor
   lcd.setCursor(0,0);
   lcd.setCursor(10,0);
-  switch (weekDay) {
+  switch (weekDay) { //Zilele saptamanii Luni-Duminica
     case 1:
     lcd.print("Lun");
     break;
